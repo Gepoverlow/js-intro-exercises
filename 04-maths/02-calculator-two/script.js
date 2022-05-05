11ee403647ad921d88d4cdf059151913bd94f217
lucas <gepeoverload@gmail.com>
2022-05-05 16:17:08 +0200

    finished second exercise of math

--- 04-maths/02-calculator-two/script.js
+++ 04-maths/02-calculator-two/script.js
@@ -9,16 +9,73 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-    // to get the value of an input: document.getElementById("element-id").value
+(function () {
+  // to get the value of an input: document.getElementById("element-id").value
 
-    var performOperation = function(operation) {
-        // perform the operation
-    };
+  function performOperation(operation) {
+    let operandOne = parseInt(document.getElementById("op-one").value);
+    let operandTwo = parseInt(document.getElementById("op-two").value);
 
-    Array.from(document.querySelectorAll("button.operator")).forEach(function($btn) {
-        $btn.addEventListener("click", function() {
-            performOperation($btn.id);
-        });
+    console.log(operation, operandOne, operandTwo);
+
+    switch (operation) {
+      case "addition":
+        add(operandOne, operandTwo);
+        break;
+      case "substraction":
+        substract(operandOne, operandTwo);
+        break;
+      case "multiplication":
+        multiply(operandOne, operandTwo);
+        break;
+      case "division":
+        divide(operandOne, operandTwo);
+        break;
+    }
+  }
+
+  function add(x, y) {
+    if (validateInput(x, y)) {
+      alert(x + y);
+    } else {
+      alert("Input cant be empty and Input should be a number!");
+    }
+  }
+  function substract(x, y) {
+    if (validateInput(x, y)) {
+      alert(x - y);
+    } else {
+      alert("Input cant be empty and Input should be a number!");
+    }
+  }
+  function multiply(x, y) {
+    if (validateInput(x, y)) {
+      alert(x * y);
+    } else {
+      alert("Input cant be empty and Input should be a number!");
+    }
+  }
+  function divide(x, y) {
+    if (y !== 0 && validateInput(x, y)) {
+      alert(x / y);
+    } else {
+      alert("You cant divide by 0!");
+    }
+  }
+
+  function validateInput(x, y) {
+    if (x !== "" && !isNaN(x) && y !== "" && !isNaN(y)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Array.from(document.querySelectorAll("button.operator")).forEach(function (
+    $btn
+  ) {
+    $btn.addEventListener("click", function () {
+      performOperation($btn.id);
     });
+  });
 })();
