09343660bf171efb6ac630a317787e63637e8957
lucas <gepeoverload@gmail.com>
2022-05-06 11:11:56 +0200

    finished exercise 3 from arrays

--- 05-arrays/03-walk-one/script.js
+++ 05-arrays/03-walk-one/script.js
@@ -9,23 +9,31 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  var fruits = [
+    "apple",
+    "pear",
+    "raspberry",
+    "tomatos",
+    "kiwi",
+    "banana",
+    "orange",
+    "mandarin",
+    "durian",
+    "peache",
+    "grap",
+    "cherry",
+  ];
 
-    var fruits = [
-        "apple",
-        "pear",
-        "raspberry",
-        "tomatos",
-        "kiwi",
-        "banana",
-        "orange",
-        "mandarin",
-        "durian",
-        "peache",
-        "grap",
-        "cherry",
-    ];
+  // your code here
 
-    // your code here
+  let button = document.getElementById("run");
 
+  button.addEventListener("click", printFruits);
+
+  function printFruits() {
+    for (let i = 0; i < fruits.length; i++) {
+      console.log(fruits[i]);
+    }
+  }
 })();
