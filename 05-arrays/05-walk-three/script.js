6bb9c287dd24d419d09a78907d0599f995c8f7fc
lucas <gepeoverload@gmail.com>
2022-05-06 11:27:17 +0200

    finished exercise 5 of arrays

--- 05-arrays/05-walk-three/script.js
+++ 05-arrays/05-walk-three/script.js
@@ -9,36 +9,40 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  var people = [
+    {
+      firstname: "Garald",
+      lastname: "Freschi",
+      email: "gfreschi0@discuz.net",
+    },
+    {
+      firstname: "Bendicty",
+      lastname: "Foster-Smith",
+      email: "bfostersmith1@mysql.com",
+    },
+    {
+      firstname: "Michelle",
+      lastname: "Draude",
+      email: "mdraude2@csmonitor.com",
+    },
+    {
+      firstname: "Cathee",
+      lastname: "Meineck",
+      email: "cmeineck3@t-online.de",
+    },
+    {
+      firstname: "Marianna",
+      lastname: "Coal",
+      email: "mcoal4@creativecommons.org",
+    },
+  ];
 
-    var people = [
-        {
-            firstname: "Garald",
-            lastname: "Freschi",
-            email: "gfreschi0@discuz.net",
-        },
-        {
-            firstname: "Bendicty",
-            lastname: "Foster-Smith",
-            email: "bfostersmith1@mysql.com",
-        },
-        {
-            firstname: "Michelle",
-            lastname: "Draude",
-            email: "mdraude2@csmonitor.com",
-        },
-        {
-            firstname: "Cathee",
-            lastname: "Meineck",
-            email: "cmeineck3@t-online.de",
-        },
-        {
-            firstname: "Marianna",
-            lastname: "Coal",
-            email: "mcoal4@creativecommons.org",
-        },
-    ];
+  // your code here
 
-    // your code here
+  const button = document.getElementById("run");
 
+  button.addEventListener("click", () => {
+    people.forEach((person) => console.log(person.firstname));
+  });
 })();
