9f49340b2e7897cae156d6a6dd909c401409c734
lucas <gepeoverload@gmail.com>
2022-05-06 10:12:36 +0200

    finished exercise 6 of maths

--- 04-maths/06-factorial-Optional/script.js
+++ 04-maths/06-factorial-Optional/script.js
@@ -9,14 +9,27 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-    
-    // to get the value of an input: document.getElementById("element-id").value
+(function () {
+  // to get the value of an input: document.getElementById("element-id").value
 
-    document.getElementById("run").addEventListener("click", function() {
+  document.getElementById("run").addEventListener("click", function () {
+    let inputNumber = document.getElementById("number").value;
 
-        // your code here
+    // your code here
 
-    });
+    function calculateFactorial(number) {
+      let current = 1;
 
+      if (number == 0 || number == 1) {
+        alert(`The factorial of ${number} is ${current}`);
+      } else {
+        for (let i = number; i >= 1; i--) {
+          current = current * i;
+        }
+        alert(`The factorial of ${number} is ${current}`);
+      }
+    }
+
+    calculateFactorial(inputNumber);
+  });
 })();
