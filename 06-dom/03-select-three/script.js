3a98a9262a54974a7660a02109634442e8b89209
lucas <gepeoverload@gmail.com>
2022-05-09 11:39:51 +0200

    finished exercise 3 of dom

--- 06-dom/03-select-three/script.js
+++ 06-dom/03-select-three/script.js
@@ -9,8 +9,10 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  // your code here
 
-    // your code here
+  const texts = document.querySelectorAll(".target");
 
+  texts.forEach((text) => (text.textContent = "Owned"));
 })();
