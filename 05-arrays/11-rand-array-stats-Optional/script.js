545259cc2f25da403a5b6577e737078197f5b232
lucas <gepeoverload@gmail.com>
2022-05-09 11:07:55 +0200

    finished exercise 11 of arrays

--- 05-arrays/11-rand-array-stats-Optional/script.js
+++ 05-arrays/11-rand-array-stats-Optional/script.js
@@ -9,8 +9,50 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  // your code here
+  const button = document.getElementById("run");
+  const numbers = document.querySelectorAll(".number");
 
-    // your code here
+  const smallest = document.getElementById("min");
+  const biggest = document.getElementById("max");
+  const total = document.getElementById("sum");
+  const average = document.getElementById("average");
 
+  button.addEventListener("click", () => {
+    let numbersArray = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+      numbersArray.push(generateNumber());
+      numbers[i].textContent = numbersArray[i];
+    }
+
+    smallest.textContent = findSmallest(numbersArray);
+    biggest.textContent = findBiggest(numbersArray);
+    total.textContent = sumAll(numbersArray);
+    average.textContent = calcAverage(numbersArray);
+  });
+
+  function generateNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+  }
+
+  function findSmallest(arr) {
+    const min = arr.reduce((a, b) => Math.min(a, b));
+    return min;
+  }
+
+  function findBiggest(arr) {
+    const max = arr.reduce((a, b) => Math.max(a, b));
+    return max;
+  }
+  function sumAll(arr) {
+    const total = arr.reduce((a, b) => a + b);
+    return total;
+  }
+  function calcAverage(arr) {
+    const total = arr.reduce((a, b) => a + b);
+    const average = total / arr.length;
+    return average;
+  }
 })();
