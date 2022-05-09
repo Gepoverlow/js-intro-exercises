3f152390d19463da76c410c00fb38549458bca97
lucas <gepeoverload@gmail.com>
2022-05-09 10:42:22 +0200

    finished exercise 10 of arrays

--- 05-arrays/10-dedupe-array-Optional/script.js
+++ 05-arrays/10-dedupe-array-Optional/script.js
@@ -9,24 +9,34 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  var fruits = [
+    "cerise",
+    "durian",
+    "pomme",
+    "poire",
+    "fraise",
+    "tomate",
+    "orange",
+    "mandarine",
+    "fraise",
+    "durian",
+    "pêche",
+    "cerise",
+    "raisin",
+    "cerise",
+  ];
 
-    var fruits = [
-        "cerise",
-        "durian",
-        "pomme",
-        "poire",
-        "fraise",
-        "tomate",
-        "orange",
-        "mandarine",
-        "fraise",
-        "durian",
-        "pêche",
-        "cerise",
-        "raisin",
-        "cerise",
-    ];
+  // your code here
 
-    // your code here
+  const button = document.getElementById("run");
+
+  button.addEventListener("click", deDupe);
+
+  function deDupe() {
+    let set = new Set(fruits);
+    let values = set.values();
+    let array = Array.from(values);
+    console.log(array);
+  }
 })();
