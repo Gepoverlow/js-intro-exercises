96d05126f840143d83a9b9f66b231b2e96d8e978
lucas <gepeoverload@gmail.com>
2022-05-10 12:42:12 +0200

    finished exercise 2 of fetch

--- 07-fetch/02-details/script.js
+++ 07-fetch/02-details/script.js
@@ -10,5 +10,69 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+  const button = document.getElementById("run");
+  const target = document.getElementById("target");
+
+  button.addEventListener("click", () => {
+    let inputId = document.getElementById("hero-id").value;
+    let inputNumber = Number(inputId);
+    getHero("http://localhost:3000/heroes", inputNumber);
+  });
+
+  async function getHero(url, id) {
+    let data = await fetch(url);
+    let response = await data.json();
+    let hArray = await response.filter((hero) => hero.id === id);
+
+    if (hArray.length !== 0) {
+      createTemplate(hArray[0].name, hArray[0].alterEgo, hArray[0].abilities);
+    } else {
+      alert("Hero was not found");
+    }
+  }
+
+  function createTemplate(name, alter, abilities) {
+    let template = document.createElement("template");
+
+    let li = document.createElement("li");
+    template.content.appendChild(li);
+
+    let h4 = document.createElement("h4");
+    li.appendChild(h4);
+
+    let strong = document.createElement("strong");
+    strong.textContent = `${name} - `;
+    h4.appendChild(strong);
+
+    let em = document.createElement("em");
+    em.textContent = alter;
+    h4.appendChild(em);
+
+    displayAbilities(abilities, li);
+
+    target.appendChild(template.content);
+  }
+
+  function displayAbilities(arr, parent) {
+    let ul = document.createElement("ul");
+    for (let i = 0; i < arr.length; i++) {
+      let para = document.createElement("li");
+      para.textContent = arr[i];
+      ul.appendChild(para);
+    }
+    parent.appendChild(ul);
+  }
 })();
+
+{
+  /* <template id="tpl-hero">
+                        <li class="hero">
+                            <h4 class="title">
+                                <strong class="name"></strong>
+                                <em class="alter-ego"></em>
+                            </h4>
+                            <p class="powers"></p>
+                        </li>
+                    </template> */
+}
