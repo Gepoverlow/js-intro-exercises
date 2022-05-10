a7b8aee065e21ea7fd75c98ac3a7529644ae87ff
lucas <gepeoverload@gmail.com>
2022-05-10 15:17:08 +0200

    finished exercise 3 of fetch

--- 07-fetch/03-list-to-template-Optional/script.js
+++ 07-fetch/03-list-to-template-Optional/script.js
@@ -10,5 +10,40 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+  const button = document.getElementById("run");
+  const target = document.getElementById("target");
+  const templateContent = document.getElementById("tpl-hero").content;
+
+  button.addEventListener("click", () => {
+    cleanNode(target);
+    getHeroes("http://localhost:3000/heroes");
+  });
+
+  async function getHeroes(url) {
+    let data = await fetch(url);
+    let response = await data.json();
+    response.forEach((resp) => handleTemplate(resp));
+  }
+
+  function handleTemplate(responseItem) {
+    let clone = templateContent.cloneNode(true);
+
+    let name = clone.querySelector(".name");
+    name.textContent = `${responseItem.name} -`;
+
+    let alter = clone.querySelector(".alter-ego");
+    alter.textContent = responseItem.alterEgo;
+
+    let powers = clone.querySelector(".powers");
+    powers.textContent = responseItem.abilities.join(", ");
+
+    target.appendChild(clone);
+  }
+
+  function cleanNode(node) {
+    while (node.firstChild) {
+      node.removeChild(node.lastChild);
+    }
+  }
 })();
