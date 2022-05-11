7a1a7c6d31eafcdfa56faf8b02e5eac00511d282
lucas <gepeoverload@gmail.com>
2022-05-11 17:27:13 +0200

    added way of dividing the string into html elements

--- optional/01-misc/01-waving-text/script.js
+++ optional/01-misc/01-waving-text/script.js
@@ -9,8 +9,54 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  // your code here
+  const target = document.getElementById("target");
+  const textContent = document.getElementById("target").textContent;
+  lettersIntoElements(textContent);
 
-    // your code here
+  function lettersIntoElements(string) {
+    let trimmed = string.trim();
+    let stringToArray = trimmed.split("");
+    handleCreation(stringToArray);
+    handleAnimation();
+  }
 
+  function handleCreation(array) {
+    let p = document.createElement("p");
+    array.forEach((item) => createEm(item, p));
+    target.appendChild(p);
+  }
+
+  function createEm(letter, parent) {
+    let em = document.createElement("em");
+    em.className = "letters";
+    em.textContent = letter;
+    em.style.display = "inline";
+    parent.appendChild(em);
+  }
+
+  function handleAnimation() {
+    let allLetters = document.querySelectorAll(".letters");
+  }
+
+  function setSizeOne(letter) {
+    letter.style.fontSize = "20px";
+  }
+
+  function setSizeTwo(letter) {
+    letter.style.fontSize = "20px";
+  }
+
+  function setSizeThree(letter) {
+    letter.style.fontSize = "20px";
+  }
+
+  function setSizeOne(letter) {
+    letter.style.fontSize = "20px";
+  }
+
+  function setSizeOne(letter) {
+    letter.style.fontSize = "20px";
+  }
 })();
