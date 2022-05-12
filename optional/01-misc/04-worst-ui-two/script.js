08a115c747c6ad666207c52559f9201318e3a522
lucas <gepeoverload@gmail.com>
2022-05-12 12:27:42 +0200

    exercise 4 of misc optionals finished but still needs cleaning up

--- optional/01-misc/04-worst-ui-two/script.js
+++ optional/01-misc/04-worst-ui-two/script.js
@@ -9,8 +9,52 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  // your code here
+  const target = document.getElementById("target");
+  const partOne = document.getElementById("part-one");
+  const partTwo = document.getElementById("part-two");
+  const partThree = document.getElementById("part-three");
+  const partFour = document.getElementById("part-four");
 
-    // your code here
+  partOne.addEventListener("click", () => {
+    updateIndividualTarget(partOne, 499);
+    updateMainTarget();
+  });
 
+  partTwo.addEventListener("click", () => {
+    updateIndividualTarget(partTwo, 99);
+    updateMainTarget();
+  });
+
+  partThree.addEventListener("click", () => {
+    updateIndividualTarget(partThree, 99);
+    updateMainTarget();
+  });
+
+  partFour.addEventListener("click", () => {
+    updateIndividualTarget(partFour, 99);
+    updateMainTarget();
+  });
+
+  function updateMainTarget() {
+    let oneValue = partOne.textContent;
+    let twoValue = partTwo.textContent;
+    let threeValue = partThree.textContent;
+    let fourValue = partFour.textContent;
+
+    target.textContent = `0${oneValue}${twoValue}${threeValue}${fourValue}`;
+  }
+
+  function updateIndividualTarget(selector, max) {
+    let currentString = selector.textContent;
+    let currentNumber = Number(currentString);
+    currentNumber++;
+
+    if (currentNumber < 10) {
+      selector.textContent = `0${currentNumber}`;
+    } else if (currentNumber <= max) {
+      selector.textContent = currentNumber;
+    }
+  }
 })();
